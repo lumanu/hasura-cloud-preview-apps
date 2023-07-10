f8d6937f21aff94f16b6ac5cffc3b7f0da47d441
Frank Wang <frank.wang@lumanu.com>
2023-07-10 11:44:39 -0400

    add debug log

--- src/previewApps.ts
+++ src/previewApps.ts
@@ -179,6 +179,11 @@ export const pollPreviewAppCreationJob = async (
       if (!successEvent) {
         throw new Error('unexpected; no job success task event')
       }
+      console.debug('pollPreviewAppCreationJob returning', {
+        projectId: successEvent.public_event_data?.projectId || '',
+        githubDeploymentJobID:
+          successEvent.public_event_data?.githubDeploymentJobID || ''
+      })
       return {
         projectId: successEvent.public_event_data?.projectId || '',
         githubDeploymentJobID:
